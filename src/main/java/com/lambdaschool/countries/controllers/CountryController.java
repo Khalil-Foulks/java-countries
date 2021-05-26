2638d051bd4e971407a87449576d01ea143c86f1
Khalil <khalilfoulks@gmail.com>
2021-05-26 18:45:29 -0400

    added http://localhost:2019/names/start/{letter} endpoint

--- src/main/java/com/lambdaschool/countries/controllers/CountryController.java
+++ src/main/java/com/lambdaschool/countries/controllers/CountryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -28,8 +29,42 @@ public class CountryController
         return new ResponseEntity<>(countries, HttpStatus.OK);
     }
 
-    // http://localhost:2019/names/start/u
+    // http://localhost:2019/names/start/{letter}
+    @GetMapping(value = "/names/start/{letter}", produces = "application/json")
+    public ResponseEntity<?> findByName(@PathVariable char letter) {
+        List<Country> countryList = new ArrayList<>();
+        countryRepository.findAll().iterator().forEachRemaining(countryList::add);
+
+        List<Country> filteredList = filterCountries(countryList, (c) -> c.getName().charAt(0) == Character.toUpperCase(letter));
+
+        return new ResponseEntity<>(filteredList, HttpStatus.OK);
+    }
+
+    private List<Country> filterCountries(List<Country> countryList, CheckCountries tester) {
+        List<Country> rtnList = new ArrayList<>();
+
+        for(Country c : countryList) {
+            if (tester.test((c))){
+                rtnList.add(c);
+            }
+        }
+        return rtnList;
+    }
+
     // http://localhost:2019/population/total
+    @GetMapping(value = "/population/total", produces = "application/json")
+    public ResponseEntity<?> getTotal() {
+        List<Country> countryList = new ArrayList<>();
+        countryRepository.findAll().iterator().forEachRemaining(countryList::add);
+
+        long total = 0;
+        for (Country c : countryList) {
+            total += c.getPopulation();
+        }
+        System.out.println("The total is: " + total);
+        return new ResponseEntity<>(total, HttpStatus.OK);
+    }
+
     // http://localhost:2019/population/min
     // http://localhost:2019/population/max
 }
